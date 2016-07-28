54c4b3a2775a96e325eba69ee64e5f2f395a1b56
Rafay Hussain <rafayrock22@gmail.com>
2016-07-28 23:51:11 +0500

    Add files via upload

---
+++ First_Sample_Page.java
@@ -0,0 +1,13 @@
+package com.example.rafay.myfirstapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class First_Sample_Page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first__sample__page);
+    }
+}
